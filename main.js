72ef7febadc41ba44c7c806384df317e105450bd
Alperen <aksoyalpi@gmail.com>
2025-05-03 06:39:55 +0200

    Refactor: Updated styles

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 const shareBtn = document.getElementById("share-icon");
 const activeFooter = document.getElementById("active-footer");
-const footer = document.getElementById("footer");
+const inactiveFooter = document.getElementById("inactive-footer");
 
 shareBtn.addEventListener("click", (e) => {
     e.target.classList.toggle("active");
-    footer.classList.toggle("hidden");
+    inactiveFooter.classList.toggle("hidden");
     activeFooter.classList.toggle("hidden");
 });
 
