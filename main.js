74a46f84ca256dc17ff1eb450557deab10b994a7
Alperen <aksoyalpi@gmail.com>
2025-05-03 07:39:07 +0200

    Finished challenge

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const activeFooter = document.getElementById("active-footer");
 const inactiveFooter = document.getElementById("inactive-footer");
 
 shareBtn.addEventListener("click", (e) => {
-    e.target.classList.toggle("active");
+    e.currentTarget.classList.toggle("active");
     inactiveFooter.classList.toggle("hidden");
     activeFooter.classList.toggle("hidden");
 });
